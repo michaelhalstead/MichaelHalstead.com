3c8fac97f6013e868956abe3d31b02c2d852a31c
Zaphod <zaphod@Zaphods-MacBook-Pro.local>
2021-07-11 15:07:57 -0700

    Removed build settings to keep Vercel intact

---
+++ dist/_nuxt/static/1626040995/blog/poetry/forty-seven/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626040995",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fpoetry\u002Fforty-seven",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
