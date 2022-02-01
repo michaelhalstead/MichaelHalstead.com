947eda49e289446e85896ed4de8f070a336340fb
Michael Halstead <redmanedigital@gmail.com>
2022-02-01 11:11:27 -0800

    Removed builds key from vercel.json. Hoping to fix hydration error

--- dist/_nuxt/static/1626040995/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fart","\u002Fblog","\u002Fblog\u002Fpoetry\u002Fforty-seven","\u002Fblog\u002Fpoetry\u002Fwhite-hair","\u002Fblog\u002Fpoetry\u002Fconclusion-and-elation"]})
---
+++ dist/_nuxt/static/1643742534/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fart","\u002Fblog","\u002Fblog\u002Fpoetry\u002Fconclusion-and-elation","\u002Fblog\u002Fpoetry\u002Fcoup-de-grace","\u002Fblog\u002Fpoetry\u002Fwhite-hair","\u002Fblog\u002Fpoetry\u002Fforty-seven"]})
